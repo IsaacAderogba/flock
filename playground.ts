9b76165c06f773f3d36cb4b6edd1a0f248aec102
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-09-28 15:31:02 +0100

    Refactor

--- playground.ts
+++
@@ -1,6 +0,0 @@
-import { I, K, C, Not, True, False } from "./src";
-
-// composing combinators
-const kite = K(I);
-
-console.log(Not(False))
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-type Boolean = typeof True | typeof False;
-
 // I := λx.x - Identity combinator
 export function Identity<T>(x: T) {
   return x;
 }
-export const I = Identity;
-export const Idiot = Identity;
 
 // M := λf.ff - Mockingbird self-application combinator
 export function SelfApplication<T extends Function>(f: T) {
   return f(f);
 }
-export const M = SelfApplication;
-export const Mockingbird = SelfApplication;
 
 // C := λfab.fba - Cardinal flip combinator
 export function Flip<T extends Function>(f: T) {
@@ -21,28 +15,20 @@ export function Flip<T extends Function>(f: T) {
       f(b)(a);
 }
 
-export const C = Flip;
-export const Cardinal = Flip;
-
 /**
  * church booleans
  */
+type Boolean = typeof True | typeof False;
 
 // K := λab.a - Kestrel constant combinator, returns first
 export function True<T>(a: T) {
   return <K>(b: K) => a;
 }
-export const K = True;
-export const Kestrel = True;
-export const First = True;
 
 // KI := λab.a - Kite inconstant combinator, returns second
 export function False<T>(a: T) {
   return <K>(b: K) => b;
 }
-export const KI = False;
-export const Kite = False;
-export const Second = False;
 
 // Not := λb.bFT - takes a boolean and tells it to select its opposite
 export function Not<T extends Boolean>(choose: T) {
