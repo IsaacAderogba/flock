d9ce3c825f259bf8074213de74be45c76fc364aa
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-09-28 14:31:49 +0100

    Add kestrel and kite combinator

---
+++ playground.ts
@@ -0,0 +1,4 @@
+import { I, K } from "./src";
+
+
+const kite = K(I)(1);
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,15 @@ export const I = <T>(x: T) => x;
 export const Idiot = I;
 
 // M := λf.ff - Mockingbird self-application combinator
-export const M = <T extends Function>(f: T) => f();
+export const M = <T extends Function>(f: T) => f(f);
 export const Mockingbird = M;
+
+// K := λab.a - Kestrel constant combinator, returns first
+export const K = <T>(a: T) => <K>(b: K) => a;
+export const Kestrel = K;
+export const True = K;
+
+// KI := λab.a - Kite inconstant combinator, returns second
+export const KI = <T>(a: T) => <K>(b: K) => b;
+export const Kite = KI;
+export const False = KI;
